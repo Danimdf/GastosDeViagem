fd3e31dd77086f700a8a2875a1ad88aeee3c8e43
Daniela Monteiro <danimdf360@gmail.com>
2020-09-15 15:25:06 -0300

    Validação final

--- app/src/main/java/com/example/gastosdeviagens/MainActivity.kt
+++ app/src/main/java/com/example/gastosdeviagens/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.gastosdeviagens
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.NumberFormatException
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -24,18 +26,33 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private fun calculate() {
         if (validationOK()) {
-            val distance = editDistance.text.toString().toFloat()
-            val price = editPrice.text.toString().toFloat()
-            val autonomy = editAutonomy.text.toString().toFloat()
 
-            val totalValue = (distance * price) / autonomy
-            textTotalValue.text = "R$ ${"%.2f".format(totalValue)}"
+            try {
+                val distance = editDistance.text.toString().toFloat()
+                val price = editPrice.text.toString().toFloat()
+                val autonomy = editAutonomy.text.toString().toFloat()
+
+                val totalValue = (distance * price) / autonomy
+                textTotalValue.text = "R$ ${"%.2f".format(totalValue)}"
+
+            } catch (nfe: NumberFormatException) {
+                Toast.makeText(applicationContext, getString(R.string.Erro_0), Toast.LENGTH_LONG)
+                    .show()
+            }
+
+        } else {
+            Toast.makeText(
+                applicationContext,
+                getString(R.string.Preencha_os_campos),
+                Toast.LENGTH_LONG
+            ).show()
         }
     }
 
     private fun validationOK(): Boolean {
         return (editDistance.text.toString() != ""
                 && editPrice.text.toString() != ""
-                && editAutonomy.text.toString() != "")
+                && editAutonomy.text.toString() != ""
+                && editAutonomy.text.toString() != "0")
     }
 }
